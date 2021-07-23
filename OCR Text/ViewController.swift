75273ed091155a4a8332ae3d82372ff760b75670
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-24 01:32:40 +0500

    commit

--- OCR Text/ViewController.swift
+++ OCR Text/ViewController.swift
@@ -12,6 +12,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // URL
+        let url = URL (string:  "https://ocr-text-extractor.p.rapidapi.com/ocr-engines/list-options")
+        guard url != nil else {
+            print("error creating url object")
+        }
+        // URL Request
+        
+        var request = URLRequest(url: <#T##URL#>, cachePolicy: <#T##URLRequest.CachePolicy#>, timeoutInterval: <#T##TimeInterval#>)
+        // Specify the Header
+        
+        // Specify The Body
+        
+        
+        // Set the request type
+        
+        
+        // Get the URL Session
+        
+        
+        // create The data task
+        
+        
+        // fire off the data task
+        
+        
+        
+        
+        
+        
     }
 
 
